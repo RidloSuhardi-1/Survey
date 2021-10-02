f28e067f45416c25b7d881d9aeefabe802952802
Ridlo Shuhardi <62533910+RidloSuhardi-1@users.noreply.github.com>
2021-10-03 00:21:59 +0700

    initial commit

---
+++ core/conn.php
@@ -0,0 +1,9 @@
+<?php
+
+    $host = "localhost"; # ini lokasi host-nya
+    $user = "root"; # ini nama user
+    $pass = "";
+    $database = "survey"; # ini untuk di database
+
+    $connect = mysqli_connect($host, $user, $pass, $database);
+?>
